fa208b75f831577b9a496f2fdcc24ff5b3661fb9
renu286 <85033863+renu286@users.noreply.github.com>
2022-10-15 13:58:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+    });
+    $('.nav-btn').click(function(){
+            $('.navbar .nav').toggleClass("active");
+            $('.nav-btn i').toggleClass("active");
+    });
+});
